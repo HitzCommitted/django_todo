cb56661ff0a40f3b5bad6a091e69e231da323cd4
HitzCommitted <hitzcommitted@gmail.com>
2024-03-06 15:59:22 +0300

    basic working todo page html

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-
-    return HttpResponse("<h1>Hello, Django!</h1>")
+    return render(request, "home.html")
