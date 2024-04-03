870da72480748084b7bf2b931864683589058062
HitzCommitted <hitzcommitted@gmail.com>
2024-04-03 22:45:24 +0300

    Added TaskAdmin

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Task
 
+
 # Register your models here.
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("task", "is_completed", "created_at", "updated_at")
+    list_filter = ("is_completed",)
+    search_fields = ("task",)
+
+
+admin.site.register(Task, TaskAdmin)
