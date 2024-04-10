4c294eb7d752192436beceaeba453fc2be7b4541
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2024-04-10 18:08:27 +0300

    working addTask Http response

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def addTask(request):
+    # return render(request, "addTask.html")
+    return HttpResponse("The form is submitted successfully.")
